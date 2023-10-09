13f13b3dd8aa3e90d165cebeb88b01774a43cab1
Philipp <Philipp.benkert@gmail.com>
2023-10-09 08:35:53 +0200

    erferf

--- data/js/scripts.js
+++ data/js/scripts.js
@@ -1,48 +1,36 @@
-
-
-function getCurrentBrowserTime() {
-    const now = new Date();
-    const timeString = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-    return timeString;
-}
-
-function updateStatus() {
-    console.log("updateStatus() aufgerufen");
-    // ... (bereits vorhandener Code) ...
-    const currentBrowserTime = getCurrentBrowserTime();
-    console.log("Aktuelle Browserzeit:", currentBrowserTime);
-    document.querySelector("#browserTime").textContent = "Browser Zeit: " + currentBrowserTime;
-}
-
 document.getElementById("currentTime").innerText = new Date().toLocaleTimeString();
 
-console.log("Script gestartet!");
+async function fetchStatus() {
+    try {
+        console.log("fetchStatus() aufgerufen");
+        
+        const response = await fetch('/get-status');
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-function fetchStatus() {
-    console.log("fetchStatus() aufgerufen");
-    fetch('/get-status')
-        .then(response => {
-            console.log("Antwort erhalten:", response);
-            return response.json();
-        })
-        .then(data => {
-            console.log("JSON-Daten:", data);
-            document.getElementById('currentTime').textContent = data.currentTime;
-            document.getElementById('wifiStatus').textContent = data.wifiStrength;
-            // ... für andere Datenpunkte, sobald Sie sie haben ...
-        })
-        .catch(error => {
-            console.error('Fehler beim Abrufen des Status:', error);
-        });
+        const data = await response.json();
+        
+        console.log("JSON-Daten:", data);
+        document.getElementById('wifiStatus').textContent = "RSSI: " + data.rssi;
+        document.getElementById('modbusStatus').textContent = data.modbusStatus;
+
+        // ... für andere Datenpunkte, sobald Sie sie haben ...
+
+    } catch (error) {
+        console.error('Fehler beim Abrufen des Status:', error);
+    }
 }
 
-// Annahme, dass eine getCurrentBrowserTime-Funktion existiert
-function getCurrentBrowserTime() {
-    console.log("getCurrentBrowserTime() aufgerufen");
-    return new Date().toLocaleTimeString();
-}
+
 
 function loadContent(url) {
     const iframe = document.getElementById("embeddedContent");
     iframe.src = url;
 }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    fetchStatus();             // Initialer Aufruf
+    setInterval(fetchStatus, 10000); // Regelmäßige Updates alle 10 Sekunden
+});
